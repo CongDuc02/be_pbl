210a2c2f46550e58acde78c1232bae5fb5313ba1
CongDuc02 <duc19042002@gmail.com>
2023-11-09 20:45:08 +0700

    be_pbl_mang

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+// const cors = require('cors');
+// const cookieParser = require('cookie-parser')
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+dotenv.config();
+
+app.use(bodyParser.json());
+
+routes(app);
+
+// console.log("process.env.MONGO_DB", process.env.MONGO_DB);
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log("Connect Db success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(port, () => {
+  console.log("Server is running in port: ", + port);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const UserRouter =require("./UserRouter")
+
+const routes = (app) => {
+app.use('/api/user', UserRouter)
+
+    // app.use('/api/user', UserRouter)
+    // app.use('/api/product', ProductRouter)
+    // app.use('/api/order', OrderRouter)
+    // app.use('/api/payment', PaymentRouter)
+}
+
+module.exports = routes
